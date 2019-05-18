b0a599a2b3746e388c1ba55f03ece973c4eae8a5
Alex-DG <alexandre.diguida@gmail.com>
2019-05-18 12:49:11 +0100

    Implementation ViewModel skeleton

--- app/src/main/java/com/alexdg/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/alexdg/countries/viewmodel/ListViewModel.kt
@@ -1,7 +1,36 @@
 package com.alexdg.countries.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
+import com.alexdg.countries.model.Country
+
 class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    var countryLoadError = MutableLiveData<Boolean>()
+    var loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData :List<Country> = listOf(Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH"),
+            Country("CountryI"),
+            Country("CountryJ")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 
 }
