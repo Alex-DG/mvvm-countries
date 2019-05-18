d60df0512b614aa338656ac95a7c8515523a1ebc
Alex-DG <alexandre.diguida@gmail.com>
2019-05-18 11:27:38 +0100

    Initial commit

---
+++ app/src/main/java/com/alexdg/countries/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.alexdg.countries.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.alexdg.countries.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/alexdg/countries/viewmodel/ListViewModel.kt
@@ -0,0 +1,7 @@
+package com.alexdg.countries.viewmodel
+
+import android.arch.lifecycle.ViewModel
+
+class ListViewModel: ViewModel() {
+
+}
